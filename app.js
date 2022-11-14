97038e602b41939c15d2b66177dc5049fa67c844
Monika <moncobra@comcast.net>
2022-11-14 12:46:58 -0800

    added render js, fetch utils, js functions

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ import { renderDogCard } from './render-utils.js';
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
+window.addEventListener('load', async () => {
+    const dogs = await getDogs();
+
+    for (let dog of dogs) {
+        const dogEl = renderDogCard(dog);
+        dogListContainer.append(dogEl);
+    }
+});
 // fetch all dogs
 // render and append all dog cards to the container
